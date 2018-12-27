b38f2d9a40c888c8ae3f7f6a31dfefcf448d7551
Glory <guangrong_xu@126.com>
2018-12-27 14:27:58 +0800

    上传模板文件
    Signed-off-by: Glory <guangrong_xu@126.com>

---
+++ src/template.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="parent">
+    <div class="child">
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'template',
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+<style>
+  .parent {
+    width:1300px;
+    height:500px;
+    border:0px solid #ddd;
+    position:relative;
+  }
+  .child {
+    width:1200px;
+    height:500px;
+    margin: auto;
+    position: absolute;
+    top: 0; left: 0; bottom: 0; right: 0;
+    background-color: #c7edcc;
+  }
+</style>
